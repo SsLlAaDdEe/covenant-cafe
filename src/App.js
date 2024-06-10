b85f9cf2d66700923f7ce74fbfdb23aa8c5cf9a2
Victory Arogundade <vickycom520@gmail.com>
2024-06-10 11:29:21 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 // src/App.js
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import StudentLogin from './StudentLogin';
 import StaffLogin from './StaffLogin';
@@ -10,15 +9,14 @@ import StaffLogin from './StaffLogin';
 const App = () => {
     return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route path="/" exact component={HomePage} />
                 <Route path="/student-login" component={StudentLogin} />
                 <Route path="/staff-login" component={StaffLogin} />
-            </Switch>
+            </Routes>
         </Router>
     );
 };
-
 
 
 export default App;

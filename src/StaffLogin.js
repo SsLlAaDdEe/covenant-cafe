46ab1299575e26b87f1d5ddfe05e784d5a88deed
Victory Arogundade <vickycom520@gmail.com>
2024-06-10 11:07:41 +0100

    Update

---
+++ src/StaffLogin.js
@@ -0,0 +1,13 @@
+// src/StaffLogin.js
+import React from 'react';
+
+const StaffLogin = () => {
+    return (
+        <div>
+            <h2>Staff Login/Sign Up</h2>
+            {/* Add your staff login/sign-up form here */}
+        </div>
+    );
+};
+
+export default StaffLogin;
---
+++ src/StudentLogin.js
@@ -0,0 +1,13 @@
+// src/StudentLogin.js
+import React from 'react';
+
+const StudentLogin = () => {
+    return (
+        <div>
+            <h2>Student Login/Sign Up</h2>
+            {/* Add your student login/sign-up form here */}
+        </div>
+    );
+};
+
+export default StudentLogin;
